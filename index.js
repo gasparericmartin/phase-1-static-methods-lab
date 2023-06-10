58845ec27397cc2cec506daf3b47308037327f3e
Eric-Martin <gaspar.ericmartin@gmail.com>
2023-06-10 12:47:54 -0400

    Complete static methods lab

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]/g, '')
+  }
+
+  static titleize(string) {
+    const badList = ['the', 'a', 'an', 'but',
+                      'of', 'and', 'for', 'at',
+                      'by', 'from']
+    const newArray = []
+    const wordArray = string.split(' ')
+    
+    if(wordArray[0].length > 1) {
+      wordArray[0].split('').splice(0, 1, wordArray[0][0].toUpperCase())
+    }
+    else {
+      wordArray.splice(0, 1, wordArray[0].toUpperCase())
+    }
+    
+    wordArray.map(word => {
+      if(!badList.find(element => element === word)) {
+        newArray.push(word[0].toUpperCase() + word.slice(1))
+      }
+      else {
+        newArray.push(word)
+      }
+    })
+
+    return newArray.join(' ')
+  }
+
 }
+
